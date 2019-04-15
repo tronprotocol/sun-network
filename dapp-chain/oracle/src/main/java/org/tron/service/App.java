852566e59997d08e4fbb08e5e27ee65aef7b2091
Heng Zhang <zhangheng1536@gmail.com>
2019-04-15 14:27:25 +0800

    merge mainChange with side Change

--- dapp-chain/oracle/src/main/java/org/tron/service/App.java
+++ dapp-chain/oracle/src/main/java/org/tron/service/App.java
@@ -3,6 +3,7 @@ package org.tron.service;
 import java.util.TimeZone;
 import lombok.extern.slf4j.Slf4j;
 import org.tron.common.config.Args;
+import org.tron.common.utils.WalletUtil;
 import org.tron.service.task.ChainTask;
 import org.tron.service.task.TaskEnum;
 
@@ -12,11 +13,9 @@ import org.tron.service.task.TaskEnum;
 @Slf4j(topic = "app")
 public class App {
 
-  private static String mainGatewayAccount = "TE5xHVk2KZZTKz81MUeh89pZzBPbFz91yN";
-  private static String mainGatewayAddress = "127.0.0.1:9092";
+  private static String mainGatewayAddress = "172.16.20.52:9092";
 
-  private static String sideChainGatewayAccount = "TE5xHVk2KZZTKz81MUeh89pZzBPbFz91yN";
-  private static String sideChainGatewayAddress = "127.0.0.1:9092";
+  private static String sideChainGatewayAddress = "172.16.22.252:9092";
 
   private static int fixedThreads = 5;
 
@@ -26,9 +25,11 @@ public class App {
     Args arg = Args.getInstance();
     arg.setParam(args);
 
-    ChainTask mainChainTask = new ChainTask(TaskEnum.MAIN_CHAIN, mainGatewayAccount,
+    ChainTask mainChainTask = new ChainTask(TaskEnum.MAIN_CHAIN,
+        WalletUtil.encode58Check(arg.getMainchainGateway()),
         mainGatewayAddress, fixedThreads);
-    ChainTask sideChainTask = new ChainTask(TaskEnum.SIDE_CHAIN, sideChainGatewayAccount,
+    ChainTask sideChainTask = new ChainTask(TaskEnum.SIDE_CHAIN,
+        WalletUtil.encode58Check(arg.getSidechainGateway()),
         sideChainGatewayAddress,
         fixedThreads);
     mainChainTask.start();
