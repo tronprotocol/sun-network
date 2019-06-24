abdc7ea64eea9a3ec83d55766f07a6863e68481d
wangming <wm18840987261@163.com>
2019-06-24 20:17:27 +0800

    add deployGatewayContract

---
+++ dapp-chain/side-chain/src/test/java/stest/tron/wallet/common/deploy/deploySideGateway.java
@@ -0,0 +1,148 @@
+package stest.tron.wallet.common.deploy;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.spongycastle.util.encoders.Hex;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.tron.api.WalletGrpc;
+import org.tron.api.WalletSolidityGrpc;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Utils;
+import org.tron.core.Wallet;
+import org.tron.protos.Protocol.Account;
+import org.tron.protos.Protocol.SmartContract;
+import org.tron.protos.Protocol.Transaction;
+import org.tron.protos.Protocol.Transaction.Result.contractResult;
+import org.tron.protos.Protocol.TransactionInfo;
+import stest.tron.wallet.common.client.Configuration;
+import stest.tron.wallet.common.client.Parameter.CommonConstant;
+import stest.tron.wallet.common.client.WalletClient;
+import stest.tron.wallet.common.client.utils.AbiUtil;
+import stest.tron.wallet.common.client.utils.Base58;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+
+@Slf4j
+public class deploySideGateway {
+
+
+  private final String testDepositTrx = Configuration.getByPath("testng.conf")
+      .getString("foundationAccount.key2");
+  private final byte[] testDepositAddress = PublicMethed.getFinalAddress(testDepositTrx);
+  private Long maxFeeLimit = Configuration.getByPath("testng.conf")
+      .getLong("defaultParameter.maxFeeLimit");
+  private String description = Configuration.getByPath("testng.conf")
+      .getString("defaultParameter.assetDescription");
+  private String url = Configuration.getByPath("testng.conf")
+      .getString("defaultParameter.assetUrl");
+  private ManagedChannel channelFull = null;
+  private WalletGrpc.WalletBlockingStub blockingStubFull = null;
+
+
+  private WalletSolidityGrpc.WalletSolidityBlockingStub blockingStubSolidity = null;
+
+  private String fullnode = Configuration.getByPath("testng.conf")
+      .getStringList("fullnode.ip.list").get(0);
+  private String fullnode1 = Configuration.getByPath("testng.conf")
+      .getStringList("fullnode.ip.list").get(1);
+
+  ECKey ecKey1 = new ECKey(Utils.getRandom());
+  byte[] depositAddress = ecKey1.getAddress();
+  String testKeyFordeposit = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
+
+  @BeforeSuite
+  public void beforeSuite() {
+    Wallet wallet = new Wallet();
+    Wallet.setAddressPreFixByte(CommonConstant.ADD_PRE_FIX_BYTE_MAINNET);
+  }
+
+  /**
+   * constructor.
+   */
+
+  @BeforeClass(enabled = true)
+  public void beforeClass() {
+//    PublicMethed.printAddress(testKeyFordeposit);
+    channelFull = ManagedChannelBuilder.forTarget(fullnode)
+        .usePlaintext(true)
+        .build();
+    blockingStubFull = WalletGrpc.newBlockingStub(channelFull);
+  }
+
+  @Test(enabled = true, description = "deploy Side Chain Gateway")
+  public void test1DepositTrc20001() {
+
+    PublicMethed.printAddress(testKeyFordeposit);
+
+    Account accountOralce = PublicMethed.queryAccount(depositAddress, blockingStubFull);
+    long OralceBalance = accountOralce.getBalance();
+    logger.info("OralceBalance: " + OralceBalance);
+
+    String contractName = "gateWaysidechainContract";
+    String code = Configuration.getByPath("testng.conf")
+        .getString("code.code_SideGateway");
+    String abi = Configuration.getByPath("testng.conf")
+        .getString("abi.abi_SideGateway");
+    String parame = "\"" + Base58.encode58Check(testDepositAddress) + "\"";
+
+    String deployTxid = PublicMethed
+        .deployContractWithConstantParame(contractName, abi, code, "constructor(address)",
+            parame, "",
+            maxFeeLimit,
+            0L, 100, null, testDepositTrx, testDepositAddress
+            , blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+    Optional<TransactionInfo> infoById = PublicMethed
+        .getTransactionInfoById(deployTxid, blockingStubFull);
+    logger.info("infoById: " + infoById);
+    byte[] mainChainGateway = infoById.get().getContractAddress().toByteArray();
+    String mainChainGatewayAddress = WalletClient.encode58Check(mainChainGateway);
+    Assert.assertEquals(0, infoById.get().getResultValue());
+    Assert.assertNotNull(mainChainGateway);
+
+    SmartContract smartContract = PublicMethed.getContract(mainChainGateway,
+        blockingStubFull);
+    Assert.assertNotNull(smartContract.getAbi());
+
+    String outputPath = "./src/test/resources/sideChainGatewayAddress" ;
+    try {
+      File mainChainFile = new File(outputPath);
+      Boolean cun = mainChainFile.createNewFile();
+      FileWriter writer = new FileWriter(mainChainFile);
+      BufferedWriter out = new BufferedWriter(writer);
+      out.write(mainChainGatewayAddress);
+
+      out.close();
+      writer.close();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+
+  }
+
+  /**
+   * constructor.
+   */
+  @AfterClass
+  public void shutdown() throws InterruptedException {
+    if (channelFull != null) {
+      channelFull.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+
+}
