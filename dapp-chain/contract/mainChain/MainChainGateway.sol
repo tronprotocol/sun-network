6855b95e44e2c9fbac21664a35b2874ec91bc35f
Heng Zhang <zhangheng1536@gmail.com>
2019-06-24 11:54:04 +0800

    change log

--- dapp-chain/contract/mainChain/MainChainGateway.sol
+++ dapp-chain/contract/mainChain/MainChainGateway.sol
@@ -78,6 +78,7 @@ contract MainChainGateway is  OracleManagerContract {
     function withdrawTRC10(address _to, trcToken tokenId, uint256 value, uint256 nonce, bytes[] oracleSigns)
     public onlyOracle()
     {
+        require(oracleSigns.length<=numOracles,"signs num > oracles num");
         require(withdrawDone[nonce] == false, "withdrawDone[nonce] != false");
         bytes32 dataHash = keccak256(abi.encodePacked(_to, tokenId, value, nonce));
         checkOracles(dataHash, nonce, oracleSigns);
@@ -91,6 +92,7 @@ contract MainChainGateway is  OracleManagerContract {
     function withdrawTRC20(address _to, address contractAddress, uint256 value, uint256 nonce, bytes[] oracleSigns)
     public onlyOracle()
     {
+        require(oracleSigns.length<=numOracles,"signs num > oracles num");
         require(withdrawDone[nonce] == false, "withdrawDone[nonce] != false");
         bytes32 dataHash = keccak256(abi.encodePacked(_to, contractAddress, value, nonce));
         checkOracles(dataHash, nonce, oracleSigns);
@@ -103,6 +105,7 @@ contract MainChainGateway is  OracleManagerContract {
     function withdrawTRC721(address _to, address contractAddress, uint256 uid, uint256 nonce, bytes[] oracleSigns)
     public onlyOracle()
     {
+        require(oracleSigns.length<=numOracles,"signs num > oracles num");
         require(withdrawDone[nonce] == false, "withdrawDone[nonce] != false");
         bytes32 dataHash = keccak256(abi.encodePacked(_to, contractAddress, uid, nonce));
         checkOracles(dataHash, nonce, oracleSigns);
@@ -116,6 +119,7 @@ contract MainChainGateway is  OracleManagerContract {
     function withdrawTRX(address _to, uint256 value, uint256 nonce, bytes[] oracleSigns)
     public onlyOracle()
     {
+        require(oracleSigns.length<=numOracles,"signs num > oracles num");
         require(withdrawDone[nonce] == false, "withdrawDone[nonce] != false");
         bytes32 dataHash = keccak256(abi.encodePacked(_to, value, nonce));
         checkOracles(dataHash, nonce, oracleSigns);
