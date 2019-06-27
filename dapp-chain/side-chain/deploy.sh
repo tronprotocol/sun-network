8f715e5988450716ef1dd11f9c186f2065b668bd
wangming <wm18840987261@163.com>
2019-06-27 18:40:23 +0800

    modify

--- dapp-chain/side-chain/deploy.sh
+++ dapp-chain/side-chain/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$TRAVIS_BRANCH" = "develop" || "$TRAVIS_BRANCH" = "master" ]];then
      stest_server=39.106.81.60
-    `ssh java-tron@$stest_server -p 22008 sh /data/workspace/docker_workspace_sideChain/do_stest_sideChain.sh`
+    `ssh java-tron@$stest_server -p 22008 sh /data/databackup/docker_workspace_sideChain/do_stest_sidechain.sh`
 fi
 echo "bye bye"
 echo $stest_server
