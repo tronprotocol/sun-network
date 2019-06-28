f56d431b4191adcee4f55ecb32f9567ac274225e
wangming <wm18840987261@163.com>
2019-06-28 19:09:08 +0800

    modify

--- dapp-chain/side-chain/deploy.sh
+++ dapp-chain/side-chain/deploy.sh
@@ -7,10 +7,10 @@ if [[ "$TRAVIS_BRANCH" = "develop" || "$TRAVIS_BRANCH" = "master" ]];then
      stest_server=39.106.81.60
      #change_branch_CMD="sed -i '1c branch_name_side=$TRAVIS_BRANCH' /data/databackup/docker_workspace_sideChain/do_stest_sidechain.sh"
      #ssh java-tron@$stest_server -p 22008 $change_branch_CMD
-     ssh java-tron@$stest_server -p 22008 sh /data/databackup/docker_workspace_sideChain/do_stest_sidechain.sh >$stestlogname
+     ssh java-tron@$stest_server -p 22008 sh /data/databackup/docker_workspace_sideChain/do_stest_sidechain.sh > $stestlogname
      echo "stest start"
-     cat $stestlogname
-     #cat $stestlogname | grep "Stest result is:" -A 10000
+     #cat $stestlogname
+     cat $stestlogname | grep "Stest result is:" -A 10000
      echo "stest end"
 fi
 echo "bye bye"
