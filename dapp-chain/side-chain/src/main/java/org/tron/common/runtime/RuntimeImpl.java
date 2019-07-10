e38bd65c12d2b4d4ba7ec1fe136d5bd028aa1b2a
taihaofu <taihao.fu@gmail.com>
2019-07-10 15:59:19 +0800

    refine logic

--- dapp-chain/side-chain/src/main/java/org/tron/common/runtime/RuntimeImpl.java
+++ dapp-chain/side-chain/src/main/java/org/tron/common/runtime/RuntimeImpl.java
@@ -372,15 +372,19 @@ public class RuntimeImpl implements Runtime {
         sunTokenCallTokenValue = tokenValue;
       }
 
-      energyLimit = getAccountEnergyLimitWithFixRatio(creator, feeLimit, callValue,
-          sunTokenCallTokenValue);
+      if (!VMConfig.isVmResourceChargingOn()) {
+        energyLimit = 10_000_000;
+      }
+      else {
+        energyLimit = getAccountEnergyLimitWithFixRatio(creator, feeLimit, callValue,
+            sunTokenCallTokenValue);
+      }
+
       if (energyLimit < 0) {
         throw new ContractValidateException("not enough energy to initialize vm");
       }
 
-      if (!VMConfig.isVmResourceChargingOn()) {
-        energyLimit = 10_000_000;
-      }
+
 
       byte[] ops = newSmartContract.getBytecode().toByteArray();
       rootInternalTransaction = new InternalTransaction(trx, trxType);
@@ -487,7 +491,11 @@ public class RuntimeImpl implements Runtime {
       long energyLimit;
       if (isStaticCall) {
         energyLimit = Constant.ENERGY_LIMIT_IN_CONSTANT_TX;
-      } else {
+      }
+      else if (!VMConfig.isVmResourceChargingOn()) {
+        energyLimit = 10_000_000;
+      }
+      else {
         AccountCapsule creator = this.deposit
             .getAccount(deployedContract.getInstance().getOriginAddress().toByteArray());
 
@@ -496,10 +504,6 @@ public class RuntimeImpl implements Runtime {
         if (energyLimit < 0) {
           throw new ContractValidateException("not enough energy to initialize vm");
         }
-      }
-
-      if (!VMConfig.isVmResourceChargingOn()) {
-        energyLimit = 10_000_000;
       }
 
       long maxCpuTimeOfOneTx = deposit.getDbManager().getDynamicPropertiesStore()
