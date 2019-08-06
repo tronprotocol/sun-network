8f7c976dc3d78e587798db13362ea3a63be2f1ae
llwslc <llwslc@gmail.com>
2019-08-06 12:26:14 +0800

    add doc path

--- documentation/index.js
+++ documentation/index.js
@@ -22,6 +22,7 @@ app.all('*', function(req, res, next) {
 
 app.use(express.json());
 app.use('/sunnetwork', express.static('docs/.vuepress/dist'));
+app.use('/sunnetwork/doc', express.static('docs/.vuepress/distdoc'));
 app.post('/sunnetwork/token', async (req, res) => {
   let to_address = '';
   let amount = 1e10;
