7a74648adbe2d8a8a12311dad2728f5d79ced615
llwslc <llwslc@gmail.com>
2019-08-10 20:23:09 +0800

    demo url

--- documentation/home/src/store.js
+++ documentation/home/src/store.js
@@ -12,8 +12,8 @@ export default new Vuex.Store({
     resourcesUrl: 'https://github.com/tronprotocol/sun-network',
     githubUrl: 'https://github.com/tronprotocol/sun-network',
     telegramUrl: 'https://t.me/sun_network',
-    demo1Url: '/sunnetwork/demo/donate',
-    demo2Url: '/sunnetwork/demo/dice',
+    demo1Url: 'http://47.252.84.158:50053/static/#/home',
+    demo2Url: 'http://47.252.84.158:50052/static/#/home',
     donateUrl: 'https://tron.network/donation?lng=en',
     faqUrl: 'https://gitter.im/tronprotocol/sun-network',
     discordUrl: 'https://discordapp.com/invite/Anun6b7'
