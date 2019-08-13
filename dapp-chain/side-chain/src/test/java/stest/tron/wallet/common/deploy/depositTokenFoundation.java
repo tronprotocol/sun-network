14debd91ac9dc5207345ffe8e7f1e78548cf391f
wangming <wm18840987261@163.com>
2019-08-13 17:12:24 +0800

    modify stest

--- dapp-chain/side-chain/src/test/java/stest/tron/wallet/common/deploy/depositTokenFoundation.java
+++ dapp-chain/side-chain/src/test/java/stest/tron/wallet/common/deploy/depositTokenFoundation.java
@@ -48,7 +48,7 @@ public class depositTokenFoundation {
       .getString("gateway_address.key1");
 
   final String ChainIdAddress = Configuration.getByPath("testng.conf")
-      .getString("gateway_address.ChainIdAddress");
+      .getString("gateway_address.chainIdAddress");
   final byte[] ChainIdAddressKey = WalletClient.decodeFromBase58Check(ChainIdAddress);
 
   final String tokenFoundationKey001 = Configuration.getByPath("testng.conf")
