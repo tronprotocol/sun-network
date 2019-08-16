0e7ad9f7c8e07bbacb4f78d7eba028f81f1e2369
wangming <wm18840987261@163.com>
2019-08-16 18:11:23 +0800

    modify travis

--- dapp-chain/side-chain/tron.sh
+++ dapp-chain/side-chain/tron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $encrypted_01022d2a4f87_key ];then
-  openssl aes-256-cbc -K $encrypted_01022d2a4f87_key -iv $encrypted_01022d2a4f87_iv -in tron.enc -out tron -d
-  cat tron > ~/.ssh/id_rsa
+  openssl aes-256-cbc -K $encrypted_01022d2a4f87_key -iv $encrypted_01022d2a4f87_iv -in sunnetworkci.enc -out tron -d
+  cat sunnetworkci > ~/.ssh/id_rsa
   chmod 600 ~/.ssh/id_rsa
   echo "Add docker server success"
   sonar-scanner
