bd614f310e3cce7916d5f46b589b42198f7c1e16
wangming <wm18840987261@163.com>
2019-08-16 18:20:42 +0800

    modify travis

--- dapp-chain/side-chain/tron.sh
+++ dapp-chain/side-chain/tron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $encrypted_01022d2a4f87_key ];then
-  openssl aes-256-cbc -K $encrypted_01022d2a4f87_key -iv $encrypted_01022d2a4f87_iv -in sunnetworkci.enc -out tron -d
+  openssl aes-256-cbc -K $encrypted_01022d2a4f87_key -iv $encrypted_01022d2a4f87_iv -in sunnetworkci.enc -out sunnetworkci -d
   cat sunnetworkci > ~/.ssh/id_rsa
   chmod 600 ~/.ssh/id_rsa
   echo "Add docker server success"
