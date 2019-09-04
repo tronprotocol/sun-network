527c81988cd02f7a4a3e4576fe4c8d8b85494b17
kunqian <kunqian@tron.network>
2019-09-04 17:55:52 +0800

    modify config

--- sun-cli/src/main/java/org/tron/walletcli/task/AccountTask.java
+++ sun-cli/src/main/java/org/tron/walletcli/task/AccountTask.java
@@ -5,10 +5,6 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tron.api.GrpcAPI.AddressPrKeyPairMessage;
@@ -23,29 +19,34 @@ public class AccountTask extends SideChainTask {
 
   private List<String> accountList = new ArrayList<>();
 
-  private AtomicInteger index = new AtomicInteger(0);
-
-  private ScheduledExecutorService service = Executors
-      .newSingleThreadScheduledExecutor();
-
 
   public void runTask(SunNetwork sdk) {
-    service.scheduleAtFixedRate(() -> sendCoinAndDeposit(sdk), 0, ConfigInfo.interval, TimeUnit.MILLISECONDS);
+    sendCoinAndDeposit(sdk);
   }
 
   public void sendCoinAndDeposit(SunNetwork sdk) {
 
     logger.info("send coin and deposit");
 
-    sdk.setPrivateKey(ConfigInfo.privateKey);
-    if (accountList.size() > 0) {
-      if (index.get() >= accountList.size()) {
-        index.set(0);
-      }
-      String accountInfo = accountList.get(index.getAndAdd(1));
-      sdk.getSideChainService().sendCoin(accountInfo.split(",")[0], ConfigInfo.contractDepositValue);
-      sdk.setPrivateKey(accountInfo.split(",")[1]);
-      triggerContract(sdk, ConfigInfo.contractDepositValue, ConfigInfo.contractDeposit);
+    while (true) {
+      sdk.setPrivateKey(ConfigInfo.privateKey);
+      accountList.forEach(account -> {
+        sdk.getSideChainService().sendCoin(account.split(",")[0], ConfigInfo.contractDepositValue);
+        try {
+          Thread.sleep(ConfigInfo.interval);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      });
+      accountList.forEach(account -> {
+        sdk.setPrivateKey(account.split(",")[1]);
+        triggerContract(sdk, ConfigInfo.contractDepositValue, ConfigInfo.contractDeposit);
+        try {
+          Thread.sleep(ConfigInfo.interval);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      });
     }
   }
 
@@ -61,12 +62,8 @@ public class AccountTask extends SideChainTask {
         SunNetworkResponse<AddressPrKeyPairMessage> resp = sdk.getMainChainService().generateAddress();
         if (resp.getCode() == ErrorCodeEnum.SUCCESS.getCode()) {
           String info = resp.getData().getAddress() + "," + resp.getData().getPrivateKey();
-
-          sdk.getSideChainService().sendCoin(resp.getData().getAddress(), ConfigInfo.accountFreezeBalance * 2);
-
-          sdk.setPrivateKey(resp.getData().getPrivateKey());
-          sdk.getSideChainService().freezeBalance(ConfigInfo.accountFreezeBalance, 3, 0, null);
-          sdk.getSideChainService().freezeBalance(ConfigInfo.accountFreezeBalance, 3, 1, null);
+          sdk.getSideChainService().createAccount(resp.getData().getAddress());
+          sdk.getSideChainService().freezeBalance(ConfigInfo.accountFreezeBalance, 3, 1, resp.getData().getAddress());
           accountList.add(info);
           out.write(info);
           out.newLine();
