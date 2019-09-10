83a259c332995cb3a15e7013c5cddf7244b0c237
taihaofu <taihao.fu@gmail.com>
2019-09-10 15:58:53 +0800

    update a new version. version name:SunNetwork-v1.0.0-199-g2e480249,version code:1112

--- dapp-chain/side-chain/src/main/java/org/tron/program/Version.java
+++ dapp-chain/side-chain/src/main/java/org/tron/program/Version.java
@@ -2,9 +2,9 @@ package org.tron.program;
 
 public class Version {
 
-  private static final String version = "1.0.0";
-  public static final String versionName = "Sun-network";
-  public static final String versionCode = "911";
+  private static final String version = "1.0.1";
+  public static final String versionName = "SunNetwork-v1.0.0-199-g2e480249";
+  public static final String versionCode = "1112";
 
   public static String getVersion() {
     return version;
