e83d398678113360b9a86c5ab3bab31be49659d9
summertanh <huijuant58@163.com>
2019-09-24 22:28:19 +0800

    add production demo url

--- documentation/home/src/store.js
+++ documentation/home/src/store.js
@@ -12,8 +12,8 @@ export default new Vuex.Store({
     resourcesUrl: 'https://github.com/tronprotocol/sun-network',
     githubUrl: 'https://github.com/tronprotocol/sun-network',
     telegramUrl: 'https://t.me/sun_network',
-    demo1Url: 'http://47.252.84.158:50053/static/#/home',
-    demo2Url: 'http://47.252.84.158:50052/static/#/home',
+    demo1Url: 'http://47.90.206.231:50057/demos/donate/static',
+    demo2Url: 'http://47.90.206.231:50058/demos/tinydice/static',
     donateUrl: 'https://tron.network/donation?lng=en',
     faqUrl: 'https://gitter.im/tronprotocol/sun-network',
     discordUrl: 'https://discordapp.com/invite/Anun6b7'
