6a100dc7ffc58a9c6dde3d5ca0e77450b0222846
Shulinkang <ShulinKang@tron.network>
2019-10-31 12:05:57 +0800

    add unittest

---
+++ dapp-chain/side-chain/src/test/java/org/tron/common/runtime/vm/UpdateContractOwnerTest.java
@@ -0,0 +1,64 @@
+package org.tron.common.runtime.vm;
+
+import com.google.protobuf.ByteString;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+import org.spongycastle.util.encoders.Hex;
+import org.testng.Assert;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.runtime.vm.PrecompiledContracts.UpdateContractOwner;
+import org.tron.core.Wallet;
+import org.tron.core.capsule.ContractCapsule;
+import org.tron.protos.Protocol.SmartContract;
+import org.tron.protos.Protocol.SmartContract.Builder;
+import stest.tron.wallet.common.client.utils.AbiUtil;
+
+@Slf4j
+public class UpdateContractOwnerTest extends VMTestBase {
+
+  private static final String METHOD_SIGN = "updatecontractowner(address,address)";
+
+
+  @Test
+  public void correctionTest() {
+
+    UpdateContractOwner contract = new UpdateContractOwner();
+
+    ECKey fakeGateWayKey = new ECKey();
+    manager.getDynamicPropertiesStore()
+        .saveSideChainGateWayList(Arrays.asList(fakeGateWayKey.getAddress()));
+
+    ECKey contractAddressKey = new ECKey();
+    ECKey previousOwnerKey = new ECKey();
+    ECKey updateOwnerKey = new ECKey();
+    Builder builder = SmartContract.newBuilder();
+    builder.setContractAddress(ByteString.copyFrom(contractAddressKey.getAddress()))
+        .setOriginAddress(ByteString.copyFrom(previousOwnerKey.getAddress()));
+    SmartContract newSmartContract = builder.build();
+    rootDeposit
+        .createContract(contractAddressKey.getAddress(), new ContractCapsule(newSmartContract));
+
+    contract.setStaticCall(false);
+    contract.setDeposit(rootDeposit);
+    contract.setCallerAddress(fakeGateWayKey.getAddress());
+
+    byte[] input = Hex.decode(AbiUtil.parseParameters(METHOD_SIGN, Arrays.asList(
+        Wallet.encode58Check(contractAddressKey.getAddress()),
+        Wallet.encode58Check(updateOwnerKey.getAddress())
+    )));
+
+    Pair<Boolean, byte[]> ret = contract.execute(input);
+
+    Assert.assertEquals(
+        Arrays.equals(rootDeposit.getContract(contractAddressKey.getAddress()).getOriginAddress(),
+            updateOwnerKey.getAddress())
+        , true
+    );
+
+
+  }
+
+
+}
