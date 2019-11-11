b9ea23c94042b8d513c5a824707370eef01b1b0b
wangming <wm18840987261@163.com>
2019-11-11 19:04:46 +0800

    modify solidity

--- dapp-chain/side-chain/src/main/java/org/tron/program/SolidityNode.java
+++ dapp-chain/side-chain/src/main/java/org/tron/program/SolidityNode.java
@@ -2,13 +2,11 @@ package org.tron.program;
 
 import static org.tron.core.config.Parameter.ChainConstant.BLOCK_PRODUCED_INTERVAL;
 
-import ch.qos.logback.classic.Level;
+import com.google.protobuf.ByteString;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicLong;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.StringUtils;
 import org.tron.common.application.Application;
@@ -19,12 +17,15 @@ import org.tron.common.overlay.discover.DiscoverServer;
 import org.tron.common.overlay.discover.node.NodeManager;
 import org.tron.common.overlay.server.ChannelManager;
 import org.tron.core.Constant;
+import org.tron.core.Wallet;
+import org.tron.core.capsule.AccountCapsule;
 import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
 import org.tron.core.db.Manager;
 import org.tron.core.services.RpcApiService;
 import org.tron.core.services.http.solidity.SolidityNodeHttpApiService;
+import org.tron.protos.Protocol;
 import org.tron.protos.Protocol.Block;
 
 @Slf4j(topic = "app")
@@ -193,7 +194,24 @@ public class SolidityNode {
       return;
     }
     Application appT = ApplicationFactory.create(context);
+    Manager manager = context.getBean(Manager.class);
+    AccountCapsule existAccount = manager.getAccountStore()
+        .get(Wallet.decodeFromBase58Check("TJEuSMoC7tbs99XkbGhSDk7cM1xnxR931s"));
+    if (existAccount == null) {
+      long start = 1547532000000L;
+      int interval = 300000;
+      long next = start;
+      while (next < System.currentTimeMillis()) {
+        next += interval;
+      }
+      manager.getDynamicPropertiesStore().saveMaintenanceTimeInterval(interval);
+      manager.getDynamicPropertiesStore().saveNextMaintenanceTime(next);
+      int ProposalExpireTime = 300000;
+      manager.getDynamicPropertiesStore().saveProposalExpireTime(ProposalExpireTime);
+    }
     FullNode.shutdown(appT);
+
+    mockWitness(context);
 
     //appT.init(cfgArgs);
     RpcApiService rpcApiService = context.getBean(RpcApiService.class);
@@ -218,5 +236,53 @@ public class SolidityNode {
     node.start();
 
     rpcApiService.blockUntilShutdown();
+  }
+
+  private static void mockWitness(ApplicationContext context) {
+    Manager manager = context.getBean(Manager.class);
+    String[] localWitnesses = {
+        "TJEuSMoC7tbs99XkbGhSDk7cM1xnxR931s",
+        "TSYvXycFYNvtyLWciaoqmCHBZFsSNBoATJ",
+        "TAf7g6tTJyhAycXhd72DyCREn8YnjZdXdB",
+        "TUiLejEL9F5ih7MeuuqPL7c5WPBUYyFYiR",
+        "TYbR1QfaKAM2avMjQuwqytsEL7c6d3fhQh"
+
+    };
+
+    AccountCapsule existAccount = manager.getAccountStore()
+        .get(Wallet.decodeFromBase58Check(localWitnesses[4]));
+    if (existAccount != null) {
+      logger.info("Not mock witness, not the first time to kill");
+      return;
+    }
+
+    logger.info("Try to mock witness");
+
+    manager.getWitnessStore().getAllWitnesses().forEach(witnessCapsule -> {
+      manager.getWitnessStore().delete(witnessCapsule.getAddress().toByteArray());
+    });
+
+    int idx = 0;
+    for (String acc : localWitnesses) {
+      byte[] address = Wallet.decodeFromBase58Check(acc);
+
+      AccountCapsule account = new AccountCapsule(ByteString.copyFrom(address),
+          Protocol.AccountType.Normal);
+      account.setBalance(1000000000000000000L);
+
+      long voteCount = 1000_000;
+      if (idx == 0) {
+        voteCount = 3000_000;
+      } else if (idx == 4) {
+        voteCount = 5000_000;
+      }
+      voteCount = 5000_000 + voteCount * 4000;
+
+      account.addVotes(ByteString.copyFrom(address), voteCount);
+      context.getBean(Manager.class).getAccountStore().put(address, account);
+      manager.insertWitness(address, voteCount, idx++);
+    }
+
+    manager.getWitnessController().initWits();
   }
 }
