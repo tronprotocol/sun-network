4b1efb69cf512de572275f19ae2888a7dc15c93b
Heng Zhang <zhangheng1536@gmail.com>
2019-11-13 16:00:28 +0800

    fix updateOwnerContract

--- dapp-chain/contract/sideChain/SideChainGateway.sol
+++ dapp-chain/contract/sideChain/SideChainGateway.sol
@@ -67,6 +67,8 @@ contract SideChainGateway is ITRC20Receiver, ITRC721Receiver, Ownable {
 
     WithdrawMsg[] userWithdrawList;
 
+    address updateOwnerContract = address(0x10002);
+
     struct SignMsg {
         mapping(address => bool) oracleSigned;
         bytes[] signs;
@@ -144,6 +146,18 @@ contract SideChainGateway is ITRC20Receiver, ITRC721Receiver, Ownable {
         }
     }
 
+    // 1. deployDAppTRC20AndMapping
+    function multiSignForDeployDAppTRC20AndMapping(address mainChainAddress, string memory name,
+        string memory symbol, uint8 decimals, address contractOwner, uint256 nonce)
+    public goDelegateCall onlyNotStop onlyOracle
+    {
+        require(mainChainAddress != sunTokenAddress, "mainChainAddress == sunTokenAddress");
+        bool needMapping = multiSignForMapping(nonce);
+        if (needMapping) {
+            deployDAppTRC20AndMapping(mainChainAddress, name, symbol, decimals, contractOwner, nonce);
+        }
+    }
+
     function deployDAppTRC20AndMapping(address mainChainAddress, string memory name,
         string memory symbol, uint8 decimals, uint256 nonce) internal
     {
@@ -155,6 +169,17 @@ contract SideChainGateway is ITRC20Receiver, ITRC721Receiver, Ownable {
         mainContractList.push(mainChainAddress);
     }
 
+    function deployDAppTRC20AndMapping(address mainChainAddress, string memory name,
+        string memory symbol, uint8 decimals, address contractOwner, uint256 nonce) internal
+    {
+        require(mainToSideContractMap[mainChainAddress] == address(0), "TRC20 contract is mapped");
+        address sideChainAddress = address(new DAppTRC20(address(this), name, symbol, decimals));
+        updateOwnerContract.call(abi.encode(sideChainAddress, contractOwner));
+        mainToSideContractMap[mainChainAddress] = sideChainAddress;
+        sideToMainContractMap[sideChainAddress] = mainChainAddress;
+        emit DeployDAppTRC20AndMapping(mainChainAddress, sideChainAddress, nonce);
+        mainContractList.push(mainChainAddress);
+    }
     // 2. deployDAppTRC721AndMapping
     function multiSignForDeployDAppTRC721AndMapping(address mainChainAddress, string memory name,
         string memory symbol, uint256 nonce)
@@ -167,11 +192,35 @@ contract SideChainGateway is ITRC20Receiver, ITRC721Receiver, Ownable {
         }
     }
 
+    // 2. deployDAppTRC721AndMapping
+    function multiSignForDeployDAppTRC721AndMapping(address mainChainAddress, string memory name,
+        string memory symbol, address contractOwner, uint256 nonce)
+    public goDelegateCall onlyNotStop onlyOracle
+    {
+        require(mainChainAddress != sunTokenAddress, "mainChainAddress == sunTokenAddress");
+        bool needMapping = multiSignForMapping(nonce);
+        if (needMapping) {
+            deployDAppTRC721AndMapping(mainChainAddress, name, symbol, contractOwner, nonce);
+        }
+    }
+
     function deployDAppTRC721AndMapping(address mainChainAddress, string memory name,
         string memory symbol, uint256 nonce) internal
     {
         require(mainToSideContractMap[mainChainAddress] == address(0), "TRC721 contract is mapped");
         address sideChainAddress = address(new DAppTRC721(address(this), name, symbol));
+        mainToSideContractMap[mainChainAddress] = sideChainAddress;
+        sideToMainContractMap[sideChainAddress] = mainChainAddress;
+        emit DeployDAppTRC721AndMapping(mainChainAddress, sideChainAddress, nonce);
+        mainContractList.push(mainChainAddress);
+    }
+
+    function deployDAppTRC721AndMapping(address mainChainAddress, string memory name,
+        string memory symbol, address contractOwner, uint256 nonce) internal
+    {
+        require(mainToSideContractMap[mainChainAddress] == address(0), "TRC721 contract is mapped");
+        address sideChainAddress = address(new DAppTRC721(address(this), name, symbol));
+        updateOwnerContract.call(abi.encode(sideChainAddress, contractOwner));
         mainToSideContractMap[mainChainAddress] = sideChainAddress;
         sideToMainContractMap[sideChainAddress] = mainChainAddress;
         emit DeployDAppTRC721AndMapping(mainChainAddress, sideChainAddress, nonce);
@@ -480,7 +529,7 @@ contract SideChainGateway is ITRC20Receiver, ITRC721Receiver, Ownable {
         if (_signMsg.success) {
             return false;
         }
-        bytes32 ret = multivalidatesign(dataHash, _signMsg.signs, _signMsg.signOracles);
+        bytes32 ret = batchvalidatesign(dataHash, _signMsg.signs, _signMsg.signOracles);
         uint256 count = countSuccess(ret);
         if (count > numOracles * 2 / 3) {
             _signMsg.success = true;
