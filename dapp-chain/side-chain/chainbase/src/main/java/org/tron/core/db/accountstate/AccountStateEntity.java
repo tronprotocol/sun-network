318705dc0fd61aceb9555bcf9a5ab356b4a6a4a4
xiangdepeng <xiangdepeng@outlook.com>
2020-06-03 13:29:25 +0800

    put AccountResource Frozen NetUsage into AccountStateEntity for account state compare

--- dapp-chain/side-chain/chainbase/src/main/java/org/tron/core/db/accountstate/AccountStateEntity.java
+++ dapp-chain/side-chain/chainbase/src/main/java/org/tron/core/db/accountstate/AccountStateEntity.java
@@ -19,6 +19,9 @@ public class AccountStateEntity {
     builder.setBalance(account.getBalance());
     builder.putAllAssetV2(account.getAssetV2Map());
     builder.setAllowance(account.getAllowance());
+    builder.setAccountResource(account.getAccountResource());
+    builder.setFrozen(0, account.getFrozen(0));
+    builder.setNetUsage(account.getNetUsage());
     this.account = builder.build();
   }
 
