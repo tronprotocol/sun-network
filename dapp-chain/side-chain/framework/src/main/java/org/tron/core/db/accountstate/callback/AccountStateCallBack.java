a0925a86b222778dc2924b59966fd6a31be3c184
neo hong <neo.hong@tron.network>
2020-06-03 11:43:01 +0800

    sun-network-1.5.0 storage、accountstate compare for testing

--- dapp-chain/side-chain/framework/src/main/java/org/tron/core/db/accountstate/callback/AccountStateCallBack.java
+++ dapp-chain/side-chain/framework/src/main/java/org/tron/core/db/accountstate/callback/AccountStateCallBack.java
@@ -118,7 +118,10 @@ public class AccountStateCallBack extends AccountStateCallBackUtils {
     }
     logger.info("[STATEHASH] block {} stateHash {}", blockCapsule.getBlockId().getNum(),
             Hex.toHexString(newRoot));
+    logger.info("[STORAGEHASH] block {} stateHash {}", blockCapsule.getBlockId().getNum(),
+            Hex.toHexString(newStorage));
     manager.getStateCompair().fill(Hex.toHexString(newRoot));
+    manager.getStateCompair().fill(Hex.toHexString(newStorage));
   }
 
   public void executeGenerateFinish() {
